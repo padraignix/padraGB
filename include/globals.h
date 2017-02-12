ddaf84e7a7d87e96a9cd54aa3ad63a48fda750d3
Padraignix <pdowning@padraignix.com>
2017-02-12 15:00:28 -0500

    Initial global definition file

---
+++ include/globals.h
@@ -0,0 +1,43 @@
+#prama once
+
+#define LCDC 0xFF40
+#define STAT 0xFF41
+#define SCY 0xFF42
+#define SCX 0xFF43
+#define LY 0xFF44
+#define LYC 0xFF45
+#define WY 0xFF4A
+#define WX 0xFF4B
+
+#define BGP 0xFF47
+#define OBP0 0xFF48
+#define OBP1 0xFF49
+
+#define DMA 0xFF46
+
+#define DIV 0xFF04
+#define TIMA 0xFF05
+#define TMA 0xFF06
+#define TAC 0xFF07
+
+#define IE 0xFFFF
+#define IF 0xFF0F
+
+#define NR10 0xFF10
+#define NR11 0xFF11
+#define NR12 0xFF12
+#define NR13 0xFF13
+#define NR14 0xFF14
+
+#define NR21 0xFF16
+#define NR22 0xFF17
+#define NR23 0xFF18
+#define NR24 0xFF19
+
+#define NR30 0xFF1A
+#define NR31 0xFF1B
+#define NR32 0xFF1C
+#define NR33 0xFF1D
+#define NR34 0xFF1E
+
+
